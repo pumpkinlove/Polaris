441172c444bdfa8d8e41969f612e8c73b3ebcba6
xu.nan <634295462@qq.com>
2017-09-04 17:30:34 +0800

    5555

--- app/src/main/java/org/ia/polaris/BaseActivity.java
+++ app/src/main/java/org/ia/polaris/BaseActivity.java
@@ -1,12 +1,13 @@
 package org.ia.polaris;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by xu.nan on 2017/8/31.
  */
 
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends AppCompatActivity {
     protected abstract void initData();
     protected abstract void initView();
 }
--- app/src/main/java/org/ia/polaris/main/view/MainActivity.java
+++ app/src/main/java/org/ia/polaris/main/view/MainActivity.java
@@ -1,18 +1,26 @@
 package org.ia.polaris.main.view;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 import org.ia.polaris.BaseActivity;
 import org.ia.polaris.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tb_main)
+    Toolbar tbMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initData();
+        initView();
     }
 
     @Override
@@ -22,6 +30,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        initToolBar();
+    }
 
+    private void initToolBar() {
+        tbMain.setTitle("首页");
+        tbMain.setTitleTextColor(getColor(R.color.white));
+        setSupportActionBar(tbMain);
     }
 }
